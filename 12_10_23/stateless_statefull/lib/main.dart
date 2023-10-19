0635225358a37f2943a2b3bb286764b9c31b0062
Haider Ali <haider.ha.ali1@gmail.com>
2023-10-19 10:17:05 +0500

    implemented a stateless counter and used a material button to update the view using a stateful widget

--- 12_10_23/stateless_statefull/lib/main.dart
+++ 12_10_23/stateless_statefull/lib/main.dart
@@ -1,68 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(MaterialApp(
-    home:Scaffold(
-      body:Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          children: [
-            Center(
-                child:
-                      StatefullCounter()
-            ),
-          ],
-        ),
+      home: Scaffold(
+          body: SafeArea(
+              child: Center(
+                  child: StatelessCounter()
+              )
+          )
       )
-
-    )
   ));
 }
-class StatelessCounter extends StatelessWidget {
-  StatelessCounter({super.key});
-  int counter=0;
-  void myIncrement() {
-    counter++;
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Text('$counter'),
-          MaterialButton(onPressed: myIncrement,child: Text("Press me"),color: Colors.blue,)
-        ],
-      ),
 
-    );
-  }
+class StatelessCounter extends StatefulWidget {
+  @override
+  State<StatelessCounter> createState() => _StatelessCounterState();
 }
 
-class StatefullCounter extends StatefulWidget {
+class _StatelessCounterState extends State<StatelessCounter> {
+  // const StatelessCounter({super.key});
+  int counter = 0;
+  Color myColor = Colors.red;
 
-  @override
-  State<StatefullCounter> createState() => _StatefullCounterState();
-
-}
-
-class _StatefullCounterState extends State<StatefullCounter> {
-  int counter=0;
   void myIncrement(){
-    counter++;
-    setState(() {
 
-    });
+    counter++;
+    myColor = Colors.yellow;
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          Text('$counter'),
-          MaterialButton(onPressed: myIncrement,child: Text("Press me"),)
-        ],
-      ),
-
+        child: Column(
+          children: [
+            DisplayText(counter: counter),
+            MaterialButton(
+                child: Text("Press Me"),
+                onPressed: (){setState(() {
+                });
+                myIncrement();},
+                color: myColor
+            )
+          ],      )
     );
+  }
+}
+
+class DisplayText extends StatelessWidget {
+  const DisplayText({required this.counter});
+
+  final int counter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("$counter");
   }
 }
