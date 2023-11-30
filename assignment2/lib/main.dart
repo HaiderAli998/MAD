2d72a3674a4fbb8b564de344cb8f56210f024b16
Haider Ali <haider.ha.ali1@gmail.com>
2023-11-30 14:12:12 +0500

    working on sqlite for assignment 2

--- assignment2/lib/main.dart
+++ assignment2/lib/main.dart
@@ -1,125 +1,152 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'SQLite App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('SQLite App'),
+        ),
+        body: MyList(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MyList extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyListState createState() => _MyListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyListState extends State<MyList> {
+  late Database database;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    openDatabase();
+  }
+
+  Future<void> openDatabase() async {
+    database = await openDatabase(
+      join(await getDatabasesPath(), 'my_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE my_items(id INTEGER PRIMARY KEY, name TEXT, designation TEXT, imageUrl TEXT)',
+        );
+      },
+      version: 1,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return FutureBuilder<void>(
+      future: openDatabase(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return FutureBuilder<List<MyItem>>(
+            future: getItems(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  List<MyItem>? items = snapshot.data;
+                  return ListView.builder(
+                    itemCount: items?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage(items![index].imageUrl),
+                        ),
+                        title: Text('Name: ${items[index].name}'),
+                        subtitle: Text('Desig: ${items[index].designation}'),
+                        trailing: ElevatedButton(
+                          onPressed: () {
+                            // Handle delete button click
+                            // You can remove the item from the database here
+                            deleteItem(items[index].id!);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.red,
+                          ),
+                          child: Text('Delete'),
+                        ),
+                      );
+                    },
+                  );
+                }
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          );
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
     );
+  }
+
+  Future<void> insertItem(MyItem item) async {
+    await database.insert(
+      'my_items',
+      item.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<MyItem>> getItems() async {
+    final List<Map<String, dynamic>> maps = await database.query('my_items');
+
+    return List.generate(maps.length, (index) {
+      return MyItem(
+        id: maps[index]['id'],
+        name: maps[index]['name'],
+        designation: maps[index]['designation'],
+        imageUrl: maps[index]['imageUrl'],
+      );
+    });
+  }
+
+  Future<void> deleteItem(int id) async {
+    await database.delete(
+      'my_items',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    setState(() {}); // Refresh the UI after deleting an item
+  }
+}
+
+class MyItem {
+  final int? id;
+  final String name;
+  final String designation;
+  final String imageUrl;
+
+  MyItem({
+    this.id,
+    required this.name,
+    required this.designation,
+    required this.imageUrl,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'designation': designation,
+      'imageUrl': imageUrl,
+    };
   }
 }
